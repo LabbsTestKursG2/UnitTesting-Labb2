37e61efebc049be501f669bc4af5aadaa4af4c61
Michael Destici <michael@shopello.se>
2016-04-20 12:06:23 +0000

    init

---
+++ Labb1_UnitTesting/Resolver.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Int32;
+
+namespace Labb1_UnitTesting
+{
+    public class Resolver
+    {
+        public int StringCount(string var)
+        {
+            return var.Length;
+        }
+
+        public bool IsWord(string input)
+        {
+            return input.All(char.IsLetter);
+        }
+
+        public bool IsNumber(string input)
+        {
+            return input.All(char.IsNumber);
+        }
+
+        public bool IsNone(string input)
+        {
+            return input.All(x => char.IsLetterOrDigit(x) || char.IsWhiteSpace(x));
+        }
+
+        public int NextPalindrome(int input)
+        {
+            if (input < 100 || input > 999)
+                throw new Exception();
+
+            for (int n = input + 1; n < MaxValue; n++)
+            {
+                int reverse = ReverseInt(n);
+
+                if (n == reverse)
+                {
+                    return reverse;
+                }
+            }
+
+            throw new Exception();
+        }
+
+        public List<int> NextPrime(int input)
+        {
+            List<int> cache = new List<int>();
+
+            for (int n = input + 1; n < MaxValue; n++)
+            {
+                if (IsPrime(n))
+                    cache.Add(n);
+
+                if (cache.Count == 3)
+                    return cache;
+            }
+
+            throw new Exception();
+        }
+
+        private bool IsPrime(int input)
+        {
+            if (input == 1)
+                return false;
+
+            if (input == 2)
+                return true;
+
+
+            if (input % 2 == 0)
+                return false;    
+
+            for (int i = 3; i < input; i += 2)
+            {
+                if (input % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private int ReverseInt(int input)
+        {
+            int number = input;
+            int reverse = 0;
+            while (number > 0)
+            {
+                int remainder = number % 10;
+                reverse = (reverse * 10) + remainder;
+                number = number / 10;
+            }
+
+            return reverse;
+        }
+    }
+}
---
+++ Labb1_UnitTesting_Test/ResolverTests.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Labb1_UnitTesting;
+
+namespace Labb1_UnitTesting_Test
+{
+    [TestClass]
+    public class ResolverTests
+    {
+        private readonly Resolver resolver = new Resolver();
+
+        [TestMethod]
+        public void StringCount_CheckLengthOfInputString()
+        {
+            string input = "michael";
+
+            int act = resolver.StringCount(input);
+            int expected = 7;
+
+            Assert.AreEqual(act, expected);
+        }
+
+        [TestMethod]
+        public void StringType_StringIsWord()
+        {
+            string input = "michael";
+
+            bool act = resolver.IsWord(input);
+            bool expected = true;
+
+            Assert.AreEqual(act, expected);
+        }
+
+        [TestMethod]
+        public void StringType_StringIsNotWord()
+        {
+            string input = "michael d";
+
+            bool act = resolver.IsWord(input);
+            bool expected = false;
+
+            Assert.AreEqual(act, expected);
+        }
+
+        [TestMethod]
+        public void StringType_StringIsNumber()
+        {
+            string input = "1000";
+
+            bool act = resolver.IsNumber(input);
+            bool expected = true;
+
+            Assert.AreEqual(act, expected);
+        }
+
+        [TestMethod]
+        public void StringType_StringIsNotNumber()
+        {
+            string input = "michael";
+
+            bool act = resolver.IsNumber(input);
+            bool expected = false;
+
+            Assert.AreEqual(act, expected);
+        }
+
+        [TestMethod]
+        public void StringType_StringIsNone()
+        {
+            string input = "michael 93";
+
+            bool act = resolver.IsNone(input);
+            bool expected = true;
+
+            Assert.AreEqual(act, expected);
+        }
+
+        [TestMethod]
+        public void StringType_StringIsNotNone()
+        {
+            string input = "michael";
+
+            bool act = resolver.IsNone(input);
+            bool expected = true;
+
+            Assert.AreEqual(act, expected);
+        }
+
+        [TestMethod]
+        public void NextPalindrome_IsCorrectNextPalindrome()
+        {
+            int input = 220;
+
+            int act = resolver.NextPalindrome(input);
+            int expected = 222;
+
+            Assert.AreEqual(act, expected);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void NextPalindrome_ShouldThrowException()
+        {
+            resolver.NextPalindrome(1000);
+        }
+
+        [TestMethod]
+        public void NextPrime_IsValidNextPrimes()
+        {
+            int input = 100;
+
+            List<int> act = resolver.NextPrime(input);
+            List<int> expected = new List<int> {101, 103, 107};
+
+            CollectionAssert.AreEqual(act, expected);
+        }
+
+        [TestMethod]
+        public void NextPrime_IsInvalidNextPrimes()
+        {
+            int input = 100;
+
+            List<int> act = resolver.NextPrime(input);
+            List<int> expected = new List<int> { 101, 105, 107 };
+
+            CollectionAssert.AreNotEqual(act, expected);
+        }
+    }
+}
